dd22f9110b13bb29a7b0625d440906a0b62543d6
minu-aswanth <minuapple@gmail.com>
2015-10-01 23:28:21 +0530

    added tour

--- client/app/app.js
+++ client/app/app.js
@@ -1,12 +1,13 @@
 'use strict';
 
-angular.module('indiaworksApp', [
+var app=angular.module('indiaworksApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
   'ui.router',
   'ui.bootstrap',
-  'angular-tour'
+  'angular-intro',
+  'ipCookie'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
@@ -52,3 +53,56 @@ angular.module('indiaworksApp', [
       });
     });
   });
+
+  app.controller('MyController', function ($scope, $timeout, ipCookie) {
+    $scope.currentStep = ipCookie('myTour') || 1;
+
+    $scope.AfterChangeEvent = function() {
+      console.log(this._currentStep + 1);
+      $scope.currentStep = this._currentStep + 1;
+      ipCookie('myTour', $scope.currentStep, { expires: 7 });
+    };
+
+    $scope.CompletedEvent = function() {
+      console.log(this._currentStep + 2);
+      $scope.currentStep = this._currentStep + 2;
+      ipCookie('myTour', $scope.currentStep, { expires: 7 });
+    };
+
+    if($scope.currentStep < 3){
+      $timeout( function(){$scope.CallMe($scope.currentStep);}, 5000);
+    }
+
+    $scope.IntroOptions = {
+        steps:[
+        {
+            step : 1,
+            element: document.querySelector('#inputField'),
+            intro: "Enter your email to know more about us",
+            position: 'left'
+        },
+        {
+            step : 2,
+            element: document.querySelector('#facebook'),
+            intro: "Like our facebook page",
+            position: 'left'
+        },
+        {
+            step : 3,
+            element: '#youtube',
+            intro: 'Subscribe to our youtube channel',
+            position: 'right'
+        }
+        ],
+        showStepNumbers: false,
+        exitOnOverlayClick: true,
+        exitOnEsc:true,
+        nextLabel: '<strong>NEXT!</strong>',
+        prevLabel: '<span style="color:green">Previous</span>',
+        skipLabel: 'Exit',
+        doneLabel: 'Thanks'
+    };
+
+    $scope.ShouldAutoStart = false;
+
+}); 
